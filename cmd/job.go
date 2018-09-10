f8e7b6fb8b0c7859c4f5e11fa9345d9b227321dc
Julien SENON <julien.senon@gmail.com>
2018-09-10 09:59:50 +0200

    Add auto discover in job

--- cmd/job.go
+++ cmd/job.go
@@ -17,6 +17,7 @@
 package cmd
 
 import (
+	"context"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -26,6 +27,8 @@ import (
 	"strings"
 
 	"github.com/jsenon/api-cni-cleanup/config"
+	k "github.com/jsenon/api-cni-cleanup/pkg/kubernetes"
+	"go.opencensus.io/trace"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -34,6 +37,7 @@ import (
 )
 
 var urlserver string
+var autodiscover bool
 
 // serveCmd represents the serve command
 var jobCmd = &cobra.Command{
@@ -65,15 +69,89 @@ var jobCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(jobCmd)
 	jobCmd.Flags().StringVar(&urlserver, "urlserver", "http://myserver1:9010/cleanup,http://myserve2:9010/cleanup", "Set URL of cni api server")
+	jobCmd.Flags().BoolVar(&autodiscover, "autodiscover", false, "Manage auto discovery of API CNI Server")
 	err := viper.BindPFlag("urlserver", jobCmd.Flags().Lookup("urlserver"))
 	if err != nil {
 		log.Error().Msgf("Error binding urlserve value: ", err.Error())
+	}
+	err = viper.BindPFlag("autodiscover", jobCmd.Flags().Lookup("autodiscover"))
+	if err != nil {
+		log.Error().Msgf("Error binding autodiscover value: ", err.Error())
 	}
 
 }
 
 // Job Contact CNI Server for cleanning
 func Job() {
+	ctx, span := trace.StartSpan(context.Background(), "(*cniserver).Job")
+	defer span.End()
+	switch discover := viper.GetBool("autodiscover"); discover {
+	case true:
+		log.Debug().Msg("Automatic discovery")
+		err := urlauto(ctx)
+		if err != nil {
+			log.Error().Msgf("Error URL Auto func: ", err.Error())
+		}
+	case false:
+		log.Debug().Msg("Manual discovery")
+		err := urlmanu(ctx)
+		if err != nil {
+			log.Error().Msgf("Error URL Manu func: ", err.Error())
+		}
+	}
+}
+
+// urlauto will discover api cni cleanup server endpoint
+func urlauto(ctx context.Context) error {
+	ctx, span := trace.StartSpan(context.Background(), "(*cniserver).urlauto")
+	defer span.End()
+	log.Debug().Msg("In func urlauto")
+	//call func PodDiscovery
+
+	pods, err := k.PodDiscovery(ctx)
+	if err != nil {
+		log.Fatal().Msg("Error listing pod")
+		return err
+	}
+	for _, n := range pods.Items {
+		if strings.Contains(n.Name, "kube-flannel") {
+			log.Debug().Msgf("PodName: %s", n.Name)
+			log.Debug().Msgf("PodIP: %s", n.Status.PodIP)
+			url := "http://" + n.Status.PodIP + ":9010" + "/cleanup"
+			log.Debug().Msgf("Contact url: %s", url)
+			client := &http.Client{}
+			req, err := http.NewRequest("POST", url, nil)
+			if err != nil {
+				log.Error().Msgf("Error on NewRequest: %s", err.Error())
+			}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Error().Msgf("Error call server %s", err.Error())
+			}
+			defer resp.Body.Close() //nolint: errcheck
+
+			log.Info().Msgf("Reponse status: %s", resp.Status)
+			log.Debug().Msgf("Response Headers: %s", resp.Header)
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Error().Msgf("Error read body %s", err.Error())
+			}
+			log.Info().Msgf("Response Body: %s", string(body))
+			err = resp.Body.Close()
+			if err != nil {
+				log.Error().Msgf("Error close body %s", err.Error())
+			}
+
+		}
+	}
+	return nil
+}
+
+// urlmanu will call API CNI Server with manual url declaration
+func urlmanu(ctx context.Context) error {
+	ctx, span := trace.StartSpan(context.Background(), "(*cniserver).urlmanu")
+	defer span.End()
+	log.Debug().Msg("In func urlmanu")
 	s := strings.Split(viper.GetString("urlserver"), ",")
 	for _, n := range s {
 		log.Debug().Msgf("Call server: %s", n)
@@ -113,4 +191,5 @@ func Job() {
 			}
 		}
 	}
+	return nil
 }
---
+++ pkg/kubernetes/discovery.go
@@ -0,0 +1,64 @@
+// Copyright © 2018 Julien SENON <julien.senon@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Manage POD Discovery
+
+package kubernetes
+
+import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"go.opencensus.io/trace"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+// PodDiscovery will send IP of API CNI CLeanup POD
+func PodDiscovery(ctx context.Context) (pods *v1.PodList, err error) {
+	ctx, span := trace.StartSpan(ctx, "(*cniserver).K8SExternal")
+	defer span.End()
+	log.Debug().Msgf("In Poddiscovery")
+
+	var client *kubernetes.Clientset
+
+	// case internal or external k8s api
+	api := viper.GetString("api")
+	switch api := api; api {
+	case "internal":
+		client, err = K8sInternal(ctx)
+		if err != nil {
+			log.Error().Msgf("Error Call client connection to k8s internal ", err.Error())
+			return nil, err
+		}
+	case "external":
+		client, err = K8SExternal(ctx)
+		if err != nil {
+			log.Error().Msgf("Error Call client connection to k8s external ", err.Error())
+			return nil, err
+		}
+	default:
+		log.Fatal().Msg("Error definition api type")
+		return nil, err
+	}
+	// List Pods interface
+	pods, err = client.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		log.Fatal().Msg("Error listing pod")
+		return nil, err
+	}
+	return pods, nil
+}
